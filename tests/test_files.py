3f1b0e518d7ca0b5516bdec07703c57d3f863300
Victor Turrisi <vt.turrisi@gmail.com>
2019-04-09 16:06:40 -0300

    added tests and new options to trainer

--- tests/test_files.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def test_classification():
-    assert subprocess.run('python tests/main_classification.py', shell=True, check=True)
-
-
-def test_autoencoder():
-    assert subprocess.run('python tests/main_autoencoder.py', shell=True, check=True)
